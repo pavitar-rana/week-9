4e6bb788badabf3bd5475e92f2df968a2f21580c
Pavitar Rana <ranapavitar14@gmail.com>
2023-08-07 11:22:39 +0530

    added files

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "cert-file.cer" ubuntu@ec2-13-48-204-171.eu-north-1.compute.amazonaws.com "sudo bash ~/deploy.sh"
+ssh -t -i "cert-file.cer" ubuntu@ec2-13-50-112-55.eu-north-1.compute.amazonaws.com	 "sudo bash ~/deploy.sh"
